5cc0afa6a1b0152b0956d674b07d076cd2ef02ce
Tyler Pfaff <tpfaff@pandora.com>
2019-08-12 03:21:11 -0700

    Remove parse method used to parse the swift method with all the data

--- app/src/main/java/com/appsontap/bernie2020/App.kt
+++ app/src/main/java/com/appsontap/bernie2020/App.kt
@@ -3,10 +3,6 @@ package com.appsontap.bernie2020
 import android.app.Application
 import android.util.Log
 import com.appsontap.bernie2020.util.TAG
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
-import java.io.BufferedReader
-import java.io.InputStreamReader
 
 /**
 Feel the Bern
@@ -21,8 +17,6 @@ class App : Application() {
 
         AppDatabase.getDatabase()
             .populateDatabase(this)
-
-        parse()
     }
 
     companion object {
@@ -30,35 +24,6 @@ class App : Application() {
 
         fun get(): App {
             return INSTANCE
-        }
-    }
-
-    fun parse() {
-        var json = JsonObject()
-        var details = JsonArray()
-        lateinit var currentLegislation : JsonObject
-
-        val p = Pattern.compile("\"([^\"]*)\"")
-
-        var reader = BufferedReader(InputStreamReader(resources.openRawResource(R.raw.details)));
-        var line = reader.readLine();
-        while (line != null) {
-//            println(line)
-            if(line.trim().startsWith("legislation_id")){
-                println("Starts with legislation_id")
-                currentLegislation = JsonObject()
-                val tokens = line.split(",")
-                val key = tokens[0]
-                val value = tokens[1]
-                currentLegislation.addProperty(key, value)
-                details.add(currentLegislation)
-
-            }
-            line = reader.readLine()
-        }
-
-        details.forEach{
-            Log.d(TAG, it.toString())
         }
     }
 }
