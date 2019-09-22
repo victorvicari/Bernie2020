44e2e9fec6c0f998cd1b3a4927931e244dca3401
Garrett Shorr <g@garrettshorr.com>
2019-09-21 23:33:09 -0700

    moved other listeners from init to bind so they function properly when doing a search

--- app/src/main/java/com/appsontap/bernie2020/legislation/LegislationViewHolder.kt
+++ app/src/main/java/com/appsontap/bernie2020/legislation/LegislationViewHolder.kt
@@ -22,27 +22,18 @@ import com.google.gson.JsonArray
 class LegislationViewHolder(itemView: View, private val uiState: UiState.ListReady) :
     RecyclerView.ViewHolder(itemView) {
 
-    init {
-        itemView.setOnClickListener {
-            if (uiState.items[adapterPosition] is Legislation) {
-                val legislation = uiState.items[adapterPosition] as Legislation
-                val args = Bundle()
-                args.putParcelable(LegislationDetailsFragment.EXTRA_KEY_LEGISLATION, legislation)
-                (itemView.context as FragmentActivity)
-                    .supportFragmentManager
-                    .beginTransaction()
-                    .replace(
-                        R.id.fragment_container,
-                        LegislationDetailsFragment.newInstance(args),
-                        LegislationDetailsFragment.TAG
-                    )
-                    .addToBackStack(LegislationDetailsFragment.TAG)
-                    .commit()
+    fun bind(legislation: Legislation, favorites: Set<String>) {
+        itemView.textview_name.text = legislation.name
+        itemView.checkbox_favorite?.isChecked = favorites.contains(legislation.id)
+        itemView.checkbox_favorite?.setOnClickListener {
+            if (itemView.checkbox_favorite.isChecked) {
+                IOHelper.addFavoriteToSharedPrefs(itemView.context, legislation.id)
+            } else {
+                IOHelper.removeFavoriteFromSharedPrefs(itemView.context, legislation.id)
             }
         }
 
         itemView.imageview_share?.setOnClickListener {
-            val legislation = uiState.items[adapterPosition] as Legislation
             val popup = PopupMenu(itemView.context, itemView.imageview_share)
             popup.inflate(R.menu.context_menu_share_plan_and_leg)
             popup.setOnMenuItemClickListener {
@@ -79,16 +70,21 @@ class LegislationViewHolder(itemView: View, private val uiState: UiState.ListRea
             }
             popup.show()
         }
-    }
 
-    fun bind(legislation: Legislation, favorites: Set<String>) {
-        itemView.textview_name.text = legislation.name
-        itemView.checkbox_favorite?.isChecked = favorites.contains(legislation.id)
-        itemView.checkbox_favorite?.setOnClickListener {
-            if (itemView.checkbox_favorite.isChecked) {
-                IOHelper.addFavoriteToSharedPrefs(itemView.context, legislation.id)
-            } else {
-                IOHelper.removeFavoriteFromSharedPrefs(itemView.context, legislation.id)
+        itemView.setOnClickListener {
+            if (uiState.items[adapterPosition] is Legislation) {
+                val args = Bundle()
+                args.putParcelable(LegislationDetailsFragment.EXTRA_KEY_LEGISLATION, legislation)
+                (itemView.context as FragmentActivity)
+                    .supportFragmentManager
+                    .beginTransaction()
+                    .replace(
+                        R.id.fragment_container,
+                        LegislationDetailsFragment.newInstance(args),
+                        LegislationDetailsFragment.TAG
+                    )
+                    .addToBackStack(LegislationDetailsFragment.TAG)
+                    .commit()
             }
         }
     }
